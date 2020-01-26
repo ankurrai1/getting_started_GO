605ea75cc576f4cb183980cd719a474fadffdb8f
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-01-26 16:49:13 +0530

    modified map examples to repo

--- json_like_map.go
+++ json_like_map.go
@@ -9,19 +9,19 @@ func main(){
   // in more simple words it is a key value pair of data
 
   edges := make(map[string]int) // here making map object where key type is string and value is int
-  fmt.println("a map :" edges ,"\n")
+  fmt.Println("a map :", edges ,"\n")
 
   edges["side"] = 5
   edges["len"] = 10
   edges["height"] = 12
   edges["thickness"] = 2
 
-  fmt.println("map after some data addition : ",edges,"\n")
+  fmt.Println("map after some data addition : ",edges,"\n")
 
-  fmt.println("Printing some specific values : ", edges["side"])
+  fmt.Println("Printing some specific values : ", edges["side"])
 
   // deleting some thing form map by key name
   delete(edges,"thickness");
 
-  fmt.println("map after deleting the thickness : ",edges,"\n")
+  fmt.Println("map after deleting the thickness : ",edges,"\n")
 }
