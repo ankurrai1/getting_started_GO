e55853bccc9b32f5ce1a7b027c36e8a1d3e458d5
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-01-26 18:20:07 +0530

    modified pointer example to repo

--- pointer.go
+++ pointer.go
@@ -8,20 +8,21 @@ func main() {
   var num int = 5
   fmt.Println(num)
 
-  var num2 int = incrimet_by_value(num)
+  var num2 int = increment_by_value(num)
   fmt.Println(num,num2) // NO change in value of num but new value is in new variable num2
 
-  incriment_by_ref(&num); // passing value by reference
+  increment_by_ref(&num); // passing value by reference
 
   fmt.Println("value is changed by ref only current num is :",num)
 
 }
 
-func incriment_by_value(x int) int {
-  return x++
+func increment_by_value(x int) int {
+  x++
+  return x
 }
 
-func incriment_by_ref(int *x)  {
+func increment_by_ref(x *int)  {
   // x will give the address of given value a * will use for de-referencing
   *x++ // getting x value and incrimenting that
   // there is no need to return any thing the variable is changed at its place only
