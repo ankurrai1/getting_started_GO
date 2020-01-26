e7d38e86dcc9809d35925b0db0fb79705f520603
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-01-26 16:10:33 +0530

    added condition example file

---
+++ conditions.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main(){
+  x := 4
+
+  // following is showing nested if statement
+  // NOTE a: there is no () around condintion
+  if x > 6 {
+     if x == 7 {
+       fmt.Println("X is 7 \n")
+     }
+  }else if x == 5 {
+    fmt.Println("X is 5")
+  }else {
+    fmt.Println("X is less then 5")
+  }
+}
