14cf08866a14e07f83f9030b3c227120ce185253
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-01-26 17:20:14 +0530

    added function example to repo

---
+++ functions.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+  "fmt"
+  "errors"
+  "math"
+)
+
+func main(){
+  res := sum(5,7)
+  fmt.Println("sum of no :" ,res, "\n")
+
+  result,err := sqrt(36)
+
+  fmt.Println(result,err);
+
+  result2,error := sqrt(-35)
+
+  fmt.Println(result2,error)
+}
+
+
+//defining a new function Sum instade of writing all code in main
+
+// basic structure is this
+
+// func sum(a int, b int)  {
+//
+// }
+
+// intermediate Structure of function in go
+// With return type
+
+func sum(a int, b int) int  {
+  return a + b
+}
+
+// Advance Structure of function in go
+// go language can return multipal values that is an advance level of go
+
+
+func sqrt(a float64) (float64,error)  {
+  if a < 0 {
+    return 0,errors.New("Not defined for negative values")
+  }
+  return math.Sqrt(a), nil
+}
