f25d2e66b3866970861172e0b853e4433cc94818
sagargarg27 <sagargarg272.com>
2020-01-27 23:43:32 +0530

    Sagar | Basic Sum & Subtraction program added to code base

---
+++ sum_sub.go
@@ -0,0 +1,11 @@
+package main
+import"fmt"
+func main(){
+var a,b int
+fmt.Println("Enter first no")
+fmt.Scanln(&a)
+fmt.Println("Enter Second no")
+fmt.Scanln(&b)
+fmt.Println("Sum==",(a+b))
+fmt.Println("Sub==",(a-b))
+}
