fa629d8f83186f89e365a909e064644fedea3726
sagargarg27 <Sagargarg272@gmail.com>
2020-01-28 22:56:07 +0530

    sagar |modified conditions

--- conditions.go
+++ conditions.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
-func main(){
-  x := 4
+func main() {
+	x := 4
 
-  // following is showing nested if statement
-  // NOTE a: there is no () around condintion
-  if x > 6 {
-     if x == 7 {
-       fmt.Println("X is 7 \n")
-     }
-  }else if x == 5 {
-    fmt.Println("X is 5")
-  }else {
-    fmt.Println("X is less then 5")
-  }
+	// following is showing nested if statement
+	// NOTE a: there is no () around condition
+	if x > 6 {
+		if x == 7 {
+			fmt.Println("X is 7 \n")
+		}
+	} else if x == 5 {
+		fmt.Println("X is 5")
+	} else {
+		fmt.Println("X is less then 5")
+	}
 }
