53af3b9c60e2787b4de106c31114741412d61d9a
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-01 23:30:31 +0530

    Ankur | modified sturct properties

--- struct.go
+++ struct.go
@@ -13,7 +13,25 @@ type person struct {
 
 func main()  {
   p := person{first_name:"Ramu",last_name:"kaka",age:100}
-  fmt.Println(p)
 
+  //structs  are passed by values
+  // you can use & to pass it by referance
+  
+  fmt.Println(p)
   fmt.Println(p.age)
+
+  // go will find the way to assign value if given in right orders
+  // without keys name as well
+  p2 := person{
+    "sagar",
+    "garg",
+    20,
+  }
+
+  fmt.Println(p2.age)
+
+
+
+  per := struct{name string; age int}{name:"sagar",age:21}
+  fmt.Println(per)
 }
