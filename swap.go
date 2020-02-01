b74baa7ba412803cc75886b12eee99af265ff5a0
unknown <premmishra17101996@gmail.com>
2020-02-01 19:01:53 +0530

    Prem | Added A Program For Swapping Two Number

---
+++ swap.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int
+
+	fmt.Println("enter a:")
+	fmt.Scanln(&a)
+	fmt.Println("enter b:")
+	fmt.Scanln(&b)
+	c = b
+	b = a
+	a = c
+	fmt.Printf("After Swapping a Is:%d\n", a)
+	fmt.Printf("After Swapping b Is:%d", b)
+}
