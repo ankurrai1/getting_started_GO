7905729688ea8c843f8a2c71e2a37456d136568e
rishabh8960 <rishabhchaurasiya007@gmail.com>
2020-02-02 10:53:45 +0530

    Rishabh | added menu driven simple calculator

---
+++ calcu.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c, d float64
+	fmt.Print("choose your option\n1 for addition\n2 for substraction\n3 for multiplication\n4 for devide\n")
+	fmt.Scanln(&d)
+	fmt.Print("enter two numbers\n")
+	fmt.Scanln(&a)
+	fmt.Scanln(&b)
+	switch d {
+	case 1:
+		c = add(a, b)
+		fmt.Print("ans is : ", c)
+	case 2:
+		c = sub(a, b)
+		fmt.Print("ans is : ", c)
+	case 3:
+		c = mul(a, b)
+		fmt.Print("ans is : ", c)
+	case 4:
+		c = dev(a, b)
+		fmt.Print("ans is : ", c)
+	}
+}
+func add(x, y float64) float64 {
+	var a float64
+	a = x + y
+	return (a)
+}
+func sub(x, y float64) float64 {
+	var a float64
+	a = x - y
+	return (a)
+}
+func mul(x, y float64) float64 {
+	var a float64
+	a = x * y
+	return (a)
+}
+func dev(x, y float64) float64 {
+	var a float64
+	a = x / y
+	return (a)
+}
