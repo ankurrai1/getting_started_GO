d0b7a0d99152b1738521f42fd0785c11414279e8
rishabh8960 <rishabhchaurasiya007@gmail.com>
2020-02-03 20:15:37 +0530

    Rishabh | added average of n numbers

---
+++ example_codes/average.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x [100]int
+	var i, s, n int
+	var avg float64
+	s = 0
+	fmt.Print("Enter the number of element you want to calculate average for : ")
+	fmt.Scanln(&n)
+	fmt.Print("enter ", n, " numbers in array\n")
+	for i = 0; i < n; i++ {
+		fmt.Scanln(&x[i])
+		s = s + x[i]
+	}
+
+	avg = float64(s) / float64(n)
+	fmt.Print("sum is = ", s, "\navg is = ", avg)
+}
