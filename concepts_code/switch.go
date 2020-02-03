5843f665d034ea57914e56e396258414ff5553a3
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-03 18:36:26 +0530

    Ankur | updated switch for multi input values

--- concepts_code/switch.go
+++ concepts_code/switch.go
@@ -4,3 +4,18 @@ package main
 import (
   "fmt"
 )
+
+func main()  {
+  num := 3
+  switch num {
+  case 1:
+    fmt.Println("num is one")
+  case 2:
+    fmt.Println("num is two")
+  case 3, 4, 5:                  // here we can pass as many as we want values if they belong to same group
+    fmt.Println("num is three")
+  default:      // default case if no case will run it will fire
+    fmt.Println("num is not 1 2 3 4 5")
+  }
+
+}
