d77a347bfc0d7ac2b343a274952bd6dde868c06e
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-17 17:30:48 +0530

    Ankur | added interface embedding example

--- concepts_code/interface2.go
+++ concepts_code/interface2.go
@@ -3,10 +3,13 @@ package main
 
 import (
   "fmt"
+  "bytes"
 )
 
 func main()  {
-
+  var wc WriterCloser = NewBufferedWriterCloser()
+  wc.Write([]byte("This line will be print in chunk of 8 and at close it will flush out all left in"))
+  wc.Close()
 }
 //first Interface
 
@@ -27,4 +30,48 @@ type WriterCloser interface {
   Closer
 }
 
-// above is same as embedding structs in sturct to impliment 
+// above is same as embedding structs in sturct to impliment
+
+//defining type on which interface will work
+
+type BufferedWriterCloser struct{
+  buffer *bytes.Buffer
+}
+
+
+func (bwc *BufferedWriterCloser) Write(data []byte)(int, error)  {
+  n , err := bwc.buffer.Write(data)
+  if err != nil {
+    return 0 ,err
+  }
+  v := make([]byte, 8)
+  for bwc.buffer.Len() > 8{
+    _,err = bwc.buffer.Read(v)
+    if err != nil {
+      return 0 ,err
+    }
+    _, err := fmt.Println(string(v))
+    if err != nil {
+      return 0 ,err
+    }
+  }
+  return n, nil
+}
+
+func (bwc *BufferedWriterCloser) Close()(error)  {
+  for bwc.buffer.Len() > 0 {
+    data := bwc.buffer.Next(8)
+    _,err := fmt.Println(string(data))
+    if err != nil {
+      return 0 ,err
+    }
+  }
+  return nil
+}
+
+
+func NewBufferedWriterCloser() *BufferedWriterCloser {
+  return &BufferedWriterCloser{
+    buffer: *bytes.NewBuffer([]bytes{})
+  }
+}
