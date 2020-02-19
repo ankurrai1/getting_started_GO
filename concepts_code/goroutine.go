18cf17d92a0c0e7c45b5eb8ef8beeea19e55bf98
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-19 14:37:59 +0530

    Added goroution exaple to repo

--- concepts_code/goroutine.go
+++ concepts_code/goroutine.go
@@ -1,1 +1,27 @@
 // A goroutine is a lightweight thread of execution it is also known as green thread.
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func f(from string) {
+    for i := 0; i < 3; i++ {
+        fmt.Println(from, ":", i)
+    }
+}
+
+// the usual way, running it synchronously.
+// but in through go routine we provide a new thread to execute concurrently.
+
+func main() {
+  f("direct")
+  go f("goroutine")
+  go func(msg string) {
+      fmt.Println(msg)
+    }("going")
+// above two function calls are running asynchronously in separate goroutines now.
+    time.Sleep(time.Second)
+    fmt.Println("done")
+}
