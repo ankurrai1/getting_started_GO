48ebf6a92e3404c54bc69a8fb9e6cc436d41e34c
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-20 18:15:32 +0530

    Added goroution description to file

--- concepts_code/goroutine.go
+++ concepts_code/goroutine.go
@@ -23,5 +23,12 @@ func main() {
     }("going")
 // above two function calls are running asynchronously in separate goroutines now.
     time.Sleep(time.Second)
+// the above sleep is needed because main function will run in its own goroutine and as soon as it completed it will come out and
+// our go routine didn't execute so we use it to hold main function for some time to make other routine may execute. 
     fmt.Println("done")
 }
+
+
+// NOTE : in other multithreading language they use os threads which Usage a different function stack for new thread
+//        but in go goroutine is a lightweight abstraction of threads and mapped with os threads and map them
+//        this will use it when processer is free to take with little stack assigned to function
