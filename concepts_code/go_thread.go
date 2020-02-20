79ec7b585203362e9d15396c792bf1b0e63f5f08
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-20 19:20:40 +0530

    added runtime threads examples and creating new threads

---
+++ concepts_code/go_thread.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "runtime"
+)
+
+func main() {
+
+    fmt.Println("Threads : ",runtime.GOMAXPROCS(-1) )// will print current threads.
+
+    runtime.GOMAXPROCS(100) // creating 100 threads
+
+    fmt.Println("Threads : ",runtime.GOMAXPROCS(-1) )// will show all threads which i created
+}
