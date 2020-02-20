216e2f0cc2d7eae9207bc2df19269b59090c55fa
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-20 18:55:38 +0530

    added channel example to repo

---
+++ concepts_code/channel.go
@@ -0,0 +1,16 @@
+// Channels are the pipes that connect concurrent goroutines. You can send values into channels
+// from one goroutine and receive those values into another goroutine.
+package main
+
+import "fmt"
+
+func main() {
+// Create a new channel with make(chan val-type). Channels are typed by the values they convey.
+    messages := make(chan string)
+
+// Send a value into a channel using the channel <- syntax.
+// Here we send "ping" to the messages channel we made above, from a new goroutine.
+    go func() { messages <- "ping" }()
+    msg := <-messages
+    fmt.Println(msg)
+}
