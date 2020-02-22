3daf33650236c40d2a42a119975340b8a14ea1d5
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-23 03:37:42 +0530

    updated channel example file with another example

--- concepts_code/channel.go
+++ concepts_code/channel.go
@@ -8,15 +8,38 @@ import (
   "sync"
 )
 
-func main() {
-// Create a new channel with make(chan val-type). Channels are typed by the values they convey.
-    messages := make(chan string)
-
-// Send a value into a channel using the channel <- syntax.
-// Here we send "ping" to the messages channel we made above, from a new goroutine.
-    go func() { messages <- "ping" }()
-    msg := <-messages
-    fmt.Println(msg)
-}
+// func main() {
+// // Create a new channel with make(chan val-type). Channels are typed by the values they convey.
+//     messages := make(chan string)
+//
+// // Send a value into a channel using the channel <- syntax.
+// // Here we send "ping" to the messages channel we made above, from a new goroutine.
+//     go func() { messages <- "ping" }()
+//     msg := <-messages
+//     fmt.Println(msg)
+// }
 
 // main function is itself a go routine so we are sending data from one go routine to another
+
+// OTHER Example is as following you have to comment out above main function to check this out.
+
+var wg = sync.WaitGroup{}
+
+func main()  {
+  cha := make(chan int) // its strictly typed it can only send data with predefined type .
+
+  wg.Add(2)
+
+  go func ()  {
+    num := <- cha  // getting value from channel
+    fmt.Println(num)
+    wg.Done()
+  }()
+
+  go func ()  {
+    cha <- 42 // sending value to channel
+    wg.Done()
+  }()
+
+  wg.Wait()
+}
