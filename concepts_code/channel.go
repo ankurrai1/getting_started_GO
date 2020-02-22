a5567e4c0e6fe879e43b752d022c27043bf36dea
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-23 03:19:52 +0530

    updated channel example file with more details

--- concepts_code/channel.go
+++ concepts_code/channel.go
@@ -1,8 +1,12 @@
 // Channels are the pipes that connect concurrent goroutines. You can send values into channels
 // from one goroutine and receive those values into another goroutine.
+// Channels are designed to taransfer data between two or more go routines.
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "sync"
+)
 
 func main() {
 // Create a new channel with make(chan val-type). Channels are typed by the values they convey.
@@ -14,3 +18,5 @@ func main() {
     msg := <-messages
     fmt.Println(msg)
 }
+
+// main function is itself a go routine so we are sending data from one go routine to another
