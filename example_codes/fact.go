c3ee410ae0b89ba74291e98a06fd448d9bad5745
Ankur rai <rai.ankur0007@gmail.com>
2020-03-22 23:46:46 +0530

    Update fact.go

--- example_codes/fact.go
+++ example_codes/fact.go
@@ -1,18 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func fact(a int) int {
-
 	if a == 1 {
 		return 1
-
 	} else {
 		return a * fact(a-1)
-
 	}
 
 }
+
 func main() {
 	var n int
 	fmt.Println("enter a num:")
