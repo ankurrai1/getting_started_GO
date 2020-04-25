933d5eab09dde1d89b2e61922bdae428a87a297a
Ankur rai <rai.ankur0007@gmail.com>
2020-04-25 23:31:03 +0530

    Update go_thread.go

--- concepts_code/go_thread.go
+++ concepts_code/go_thread.go
@@ -6,10 +6,7 @@ import (
 )
 
 func main() {
-
     fmt.Println("Threads : ",runtime.GOMAXPROCS(-1) )// will print current threads.
-
     runtime.GOMAXPROCS(100) // creating 100 threads
-
     fmt.Println("Threads : ",runtime.GOMAXPROCS(-1) )// will show all threads which i created
 }
